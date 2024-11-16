918abc6a5f91823afb8a4ed9301377403b2f3c12
nataliareyram <a01352603@tec.mx>
2024-11-16 13:09:48 -0600

    Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,14 +1,31 @@
 #include "Customer.h"
-#include <iostream>
 
-Customer::Customer(std::string id, std::string name, int phone)
-    : customerID(id), name(name), phoneNumber(phone) {}
+// Constructor
+Customer::Customer(const std::string& id, const std::string& customerName, int phone)
+    : customerID(id), name(customerName), phoneNumber(phone) {}
 
-void Customer::displayCustomerInfo() {
-    std::cout << "ID: " << customerID << "\nName: " << name 
-              << "\nPhone: " << phoneNumber << std::endl;
+// Getters
+std::string Customer::getCustomerID() const {
+    return customerID;
 }
 
-double Customer::getTotalBalance() {
-    return 0.0; 
+std::string Customer::getName() const {
+    return name;
+}
+
+int Customer::getPhoneNumber() const {
+    return phoneNumber;
+}
+
+// Setters
+void Customer::setCustomerID(const std::string& id) {
+    customerID = id;
+}
+
+void Customer::setName(const std::string& customerName) {
+    name = customerName;
+}
+
+void Customer::setPhoneNumber(int phone) {
+    phoneNumber = phone;
 }
