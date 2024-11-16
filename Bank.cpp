0542a617fdbcc995e02fc5a4b2a5707709e79069
nataliareyram <a01352603@tec.mx>
2024-11-16 13:15:04 -0600

    Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -1,18 +1,46 @@
 #include "Bank.h"
-#include <iostream>
+#include <iostream> 
 
-Bank::Bank(std::string name, std::string loc)
+Bank::Bank(const std::string& name, const std::string& loc)
     : bankName(name), location(loc) {}
 
-void Bank::addCustomer(Customer customer) {
+std::string Bank::getBankName() const {
+    return bankName;
 }
 
-void Bank::removeCustomer(int customerID) {
+std::string Bank::getLocation() const {
+    return location;
 }
 
-void Bank::getCustomerInfo(int customerID) {
+void Bank::setBankName(const std::string& name) {
+    bankName = name;
 }
 
-void Bank::getBankDetails() {
-    std::cout << "Bank: " << bankName << "\nLocation: " << location << std::endl;
+void Bank::setLocation(const std::string& loc) {
+    location = loc;
+}
+
+void Bank::addCustomer(const Customer& customer) {
+    customers.push_back(customer);
+}
+
+void Bank::removeCustomer(const std::string& customerID) {
+    for (size_t i = 0; i < customers.size(); ++i) {
+        if (customers[i].getCustomerID() == customerID) {
+            customers.erase(customers.begin() + i);
+            return; 
+        }
+    }
+}
+
+Customer Bank::getCustomerInfo(const std::string& customerID) const {
+    for (const auto& customer : customers) {
+        if (customer.getCustomerID() == customerID) {
+            return customer;
+        }
+    }
+
+    std::cout << "Customer not found! Try again." << std::endl;
+
+    return Customer("", "", 0);
 }
