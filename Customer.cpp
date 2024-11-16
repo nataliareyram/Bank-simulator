7cc4d846017502af4378c844d4a6f9762629f8a6
nataliareyram <a01352603@tec.mx>
2024-11-16 13:15:37 -0600

    Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,10 +1,8 @@
 #include "Customer.h"
 
-// Constructor
 Customer::Customer(const std::string& id, const std::string& customerName, int phone)
     : customerID(id), name(customerName), phoneNumber(phone) {}
 
-// Getters
 std::string Customer::getCustomerID() const {
     return customerID;
 }
@@ -17,7 +15,6 @@ int Customer::getPhoneNumber() const {
     return phoneNumber;
 }
 
-// Setters
 void Customer::setCustomerID(const std::string& id) {
     customerID = id;
 }
