147f728fb7816787fdc6ed2b90002d6d018f62e9
nataliareyram <a01352603@tec.mx>
2024-11-16 13:16:48 -0600

    BankAccount.h

--- BankAccount.h
+++ BankAccount.h
@@ -4,19 +4,28 @@
 #include <string>
 
 class BankAccount {
-protected:
-    int accountNumber;
+private:
+    std::string accountNumber;
     double balance;
     std::string ownerName;
     std::string accountType;
 
 public:
-    BankAccount(int number, double bal, std::string owner, std::string type);
+    BankAccount(const std::string& accNum, double bal, const std::string& owner, const std::string& type);
+
+    std::string getAccountNumber() const;
+    double getBalance() const;
+    std::string getOwnerName() const;
+    std::string getAccountType() const;
+
+    void setAccountNumber(const std::string& accNum);
+    void setBalance(double bal);
+    void setOwnerName(const std::string& owner);
+    void setAccountType(const std::string& type);
 
     void deposit(double amount);
     void withdraw(double amount);
-    void checkBalance() const;
-    void displayAccountInfo() const;
+    std::string displayAccountInfo() const;
 };
 
-#endif 
+#endif // BANKACCOUNT_H
