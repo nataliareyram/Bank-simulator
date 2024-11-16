faa0705aa89c0d53834bf7dc775f73696c944810
nataliareyram <a01352603@tec.mx>
2024-11-16 13:11:58 -0600

    Bank.h

--- Bank.h
+++ Bank.h
@@ -2,18 +2,27 @@
 #define BANK_H
 
 #include <string>
+#include <vector>
 #include "Customer.h"
 
 class Bank {
 private:
     std::string bankName;
     std::string location;
+    std::vector<Customer> customers;
 
 public:
-    Bank(std::string name, std::string loc);
+    Bank(const std::string& name, const std::string& loc);
 
-    void addCustomer(Customer customer);
-    void removeCustomer(int customerID);
-    void getCustomerInfo(int customerID);
-    void getBankDetails();
+    std::string getBankName() const;
+    std::string getLocation() const;
+
+    void setBankName(const std::string& name);
+    void setLocation(const std::string& loc);
+
+    void addCustomer(const Customer& customer);
+    void removeCustomer(const std::string& customerID);
+    Customer getCustomerInfo(const std::string& customerID) const;
 };
+
+#endif // BANK_H
