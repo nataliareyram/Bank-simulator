2f5137b9f0c5b0481e07db56f1bbdd668b66265b
nataliareyram <a01352603@tec.mx>
2024-11-21 23:19:01 -0600

    Bank.cpp

--- Bank.cpp
+++ Bank.cpp
@@ -1,52 +1,42 @@
 #include "Bank.h"
+#include <algorithm>
 #include <stdexcept>
 
-Bank::Bank(const std::string& name, const std::string& loc)
-    : bankName(name), location(loc) {}
+Bank::Bank(std::string bankName, std::string location) 
+    : bankName(bankName), location(location) {}
 
 std::string Bank::getBankName() const {
     return bankName;
+}
+
+void Bank::setBankName(std::string bankName) {
+    bankName = bankName;
 }
 
 std::string Bank::getLocation() const {
     return location;
 }
 
-void Bank::setBankName(const std::string& name) {
-    bankName = name;
+void Bank::setLocation(std::string location) {
+    location = location;
 }
 
-void Bank::setLocation(const std::string& loc) {
-    location = loc;
-}
-
-void Bank::addCustomer(const Customer& customer) {
-    customers.push_back(customer);
-}
-
-void Bank::removeCustomer(const std::string& customerID) {
-    for (size_t i = 0; i < customers.size(); ++i) {
-        if (customers[i].getCustomerID() == customerID) {
-            customers.erase(customers.begin() + i);
-            return; 
-        }
-    }
-    throw std::runtime_error("Customer with ID " + customerID + " not found!");
-}
-
-Customer Bank::getCustomerInfo(const std::string& customerID) const {
+Customer Bank::getCustomer(std::string customerID) const {
     for (const auto& customer : customers) {
         if (customer.getCustomerID() == customerID) {
             return customer;
         }
     }
-    throw std::runtime_error("Customer with ID " + customerID + " not found!");
+    throw std::runtime_error("Customer not found");
 }
 
-void Bank::addBankAccount(const BankAccount& bankAccount) {
-    bankAccounts.push_back(bankAccount);
+void Bank::addCustomer(Customer customer) {
+    customers.push_back(customer);
 }
 
-void Bank::addInvestmentAccount(const InvestmentAccount& investmentAccount) {
-    investmentAccounts.push_back(investmentAccount);
+void Bank::removeCustomer(std::string customerID) {
+    customers.erase(std::remove_if(customers.begin(), customers.end(), 
+        [&customerID](Customer& c) {
+            return c.getCustomerID() == customerID;
+        }), customers.end());
 }
