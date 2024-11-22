cbf1a2c6ac101d016ac1480ba741c42639f78496
nataliareyram <a01352603@tec.mx>
2024-11-21 23:26:56 -0600

    Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -1,20 +1,20 @@
 #include "Card.h"
 
-Card::Card(const std::string& credit, const std::string& debit)
+Card::Card(std::string credit, std::string debit)
     : credit(credit), debit(debit) {}
 
 std::string Card::getCredit() const {
     return credit;
 }
 
-void Card::setCredit(const std::string& credit) {
-    this->credit = credit;
+void Card::setCredit(std::string credit) {
+    credit = credit;
 }
 
 std::string Card::getDebit() const {
     return debit;
 }
 
-void Card::setDebit(const std::string& debit) {
-    this->debit = debit;
+void Card::setDebit(std::string debit) {
+    debit = debit;
 }
