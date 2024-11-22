e0f4d16b2c6253456bff6c6a86cb3609aa1a3f4b
nataliareyram <a01352603@tec.mx>
2024-11-21 23:20:42 -0600

    Customer.h

--- Customer.h
+++ Customer.h
@@ -16,21 +16,26 @@ private:
     Card cardType;
 
 public:
-    Customer(const std::string& customerID, const std::string& name, int phoneNumber,
-             const BankAccount& bankAcc, const InvestmentAccount& investAcc, const Card& cardType);
+    Customer(std::string customerID, std::string name, int phoneNumber, 
+             BankAccount bankAcc, InvestmentAccount investAcc, Card cardType);
 
     std::string getCustomerID() const;
-    void setCustomerID(const std::string& customerID);
+    void setCustomerID(std::string customerID);
 
     std::string getName() const;
-    void setName(const std::string& name);
+    void setName(std::string name);
 
     int getPhoneNumber() const;
     void setPhoneNumber(int phoneNumber);
 
-    void setBankAccount(const BankAccount& bankAcc);
-    void setInvestmentAccount(const InvestmentAccount& investAcc);
-    void setCard(const Card& card);
+    BankAccount getBankAccount() const;
+    void setBankAccount(BankAccount bankAcc);
+
+    InvestmentAccount getInvestmentAccount() const;
+    void setInvestmentAccount(InvestmentAccount investAcc);
+
+    Card getCard() const;
+    void setCard(Card cardType);
 };
 
-#endif // CUSTOMER_H
+#endif
