2e1ae678b4aeda30bac5598723589287250fa380
nataliareyram <a01352603@tec.mx>
2024-11-21 18:47:36 -0600

    Customer.h

--- Customer.h
+++ Customer.h
@@ -2,25 +2,35 @@
 #define CUSTOMER_H
 
 #include <string>
+#include "BankAccount.h"
+#include "InvestmentAccount.h"
+#include "Card.h"
 
 class Customer {
 private:
     std::string customerID;
     std::string name;
     int phoneNumber;
+    BankAccount bankAcc;
+    InvestmentAccount investAcc;
+    Card cardType;
 
 public:
-    Customer(const std::string& customerID, const std::string& name, int phoneNumber);
+    Customer(const std::string& customerID, const std::string& name, int phoneNumber,
+             const BankAccount& bankAcc, const InvestmentAccount& investAcc, const Card& cardType);
 
     std::string getCustomerID() const;
-    std::string getName() const;
-    int getPhoneNumber() const;
-
     void setCustomerID(const std::string& customerID);
+
+    std::string getName() const;
     void setName(const std::string& name);
+
+    int getPhoneNumber() const;
     void setPhoneNumber(int phoneNumber);
 
-    void displayCustomerInfo() const;
+    void setBankAccount(const BankAccount& bankAcc);
+    void setInvestmentAccount(const InvestmentAccount& investAcc);
+    void setCard(const Card& card);
 };
 
 #endif // CUSTOMER_H
