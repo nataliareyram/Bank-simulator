548634706b73b8aa33b40c8dea4a60d8cff0e8ec
nataliareyram <a01352603@tec.mx>
2024-11-21 22:00:37 -0600

    Bank.h

--- Bank.h
+++ Bank.h
@@ -9,20 +9,25 @@ class Bank {
 private:
     std::string bankName;
     std::string location;
-    std::vector<Customer> customers;
+    std::vector<Customer> customers; 
 
 public:
-    Bank(const std::string& bankName, const std::string& location);
+    // These is the main constructor for the class.
+    Bank(const std::string& name, const std::string& loc);
 
+    // These are the getters for the Bank name and location.
     std::string getBankName() const;
-    void setBankName(const std::string& bankName);
-
     std::string getLocation() const;
-    void setLocation(const std::string& location);
 
-    Customer getCustomer(const std::string& customerID) const;
+    // These are the setters for Bank name and location.
+    void setBankName(const std::string& name);
+    void setLocation(const std::string& loc);
+
+    // These additional methods help add, remove, or get the information of a customer.
     void addCustomer(const Customer& customer);
     void removeCustomer(const std::string& customerID);
+    Customer getCustomer(const std::string& customerID) const;
 };
 
-#endif // BANK_H
+#endif
+
