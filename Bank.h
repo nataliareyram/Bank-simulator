fe19fcbbbee9f8a56eed0cdbbc4ff0c3830868c4
nataliareyram <a01352603@tec.mx>
2024-11-21 18:38:34 -0600

    Bank.h

--- Bank.h
+++ Bank.h
@@ -4,32 +4,25 @@
 #include <string>
 #include <vector>
 #include "Customer.h"
-#include "BankAccount.h"
-#include "InvestmentAccount.h"
 
 class Bank {
 private:
     std::string bankName;
     std::string location;
     std::vector<Customer> customers;
-    std::vector<BankAccount*> bankAccounts;  
-    std::vector<InvestmentAccount*> investmentAccounts;  
 
 public:
-    Bank(const std::string& name, const std::string& loc);
+    Bank(const std::string& bankName, const std::string& location);
 
     std::string getBankName() const;
+    void setBankName(const std::string& bankName);
+
     std::string getLocation() const;
+    void setLocation(const std::string& location);
 
-    void setBankName(const std::string& name);
-    void setLocation(const std::string& loc);
-
+    Customer getCustomer(const std::string& customerID) const;
     void addCustomer(const Customer& customer);
     void removeCustomer(const std::string& customerID);
-    Customer getCustomerInfo(const std::string& customerID) const;
-
-    void addBankAccount(BankAccount* bankAccount);
-    void addInvestmentAccount(InvestmentAccount* investmentAccount);
 };
 
 #endif // BANK_H
