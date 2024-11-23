0ac69bea094069e1e9ec765d54cd2e9f9d3fb4cb
nataliareyram <a01352603@tec.mx>
2024-11-22 21:22:55 -0600

    BankAccount.h

--- BankAccount.h
+++ BankAccount.h
@@ -1,18 +1,17 @@
 #ifndef BANKACCOUNT_H
 #define BANKACCOUNT_H
-
 #include <string>
-
+//Protected attributes along with their data tyoes
 class BankAccount {
 protected:
     std::string accountNumber;
     double balance;
     std::string ownerName;
     std::string accountType;
-
+//Main constructor that includes the aforementioned attributes defined by their data type.
 public:
     BankAccount(std::string accountNumber, double balance, std::string ownerName, std::string accountType);
-
+    //Getters and setters are paired by method to keep an organized process.
     std::string getAccountNumber() const;
     void setAccountNumber(std::string accountNumber);
 
@@ -24,7 +23,7 @@ public:
 
     std::string getAccountType() const;
     void setAccountType(std::string accountType);
-
+    //These two independent methods are meant to perform operations, not get or set information.
     void deposit(double amount);
     void withdraw(double amount);
 };
