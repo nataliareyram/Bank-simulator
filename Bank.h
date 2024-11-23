e60b18622ca26932e3cc3fa74d431707fcc7fd5d
nataliareyram <a01352603@tec.mx>
2024-11-23 08:54:40 -0600

    Bank.h

--- Bank.h
+++ Bank.h
@@ -12,19 +12,19 @@ private:
     std::vector<Customer> customers;
 public:
     // This is the main constructor for the class.
-    Bank(const std::string& bankName, const std::string& location);
+    Bank(const std::string bankName, const std::string location);
 
     // These are the coupled getters and setters for the attributes.
     std::string getBankName() const;
-    void setBankName(const std::string& bankName);
+    void setBankName(const std::string bankName);
 
     std::string getLocation() const;
-    void setLocation(const std::string& location);
+    void setLocation(const std::string location);
 
     // These additional methods help add, remove, or get the information of a customer.
-    Customer getCustomer(const std::string& customerID) const;
-    void addCustomer(const Customer& customer);
-    void removeCustomer(const std::string& customerID);
+    Customer getCustomer(const std::string customerID) const;
+    void addCustomer(const Customer customer);
+    void removeCustomer(const std::string customerID);
 };
 
 #endif
