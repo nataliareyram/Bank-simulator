633813fcaf26b0094c5866dc49ea0fc0b5252835
nataliareyram <a01352603@tec.mx>
2024-11-22 21:27:07 -0600

    Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -1,8 +1,8 @@
 #include "Card.h"
-
+//The constructor that receives the information defined by their data types.
 Card::Card(std::string credit, std::string debit)
     : credit(credit), debit(debit) {}
-
+//Paired getters and setters for the type of cards available, and their return values.
 std::string Card::getCredit() const {
     return credit;
 }
