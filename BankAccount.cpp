274c990ecffaded7b863ec8c911c0953057325a4
nataliareyram <a01352603@tec.mx>
2024-11-22 21:19:15 -0600

    BankAccount.cpp

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,9 +1,10 @@
 #include "BankAccount.h"
 #include <stdexcept>
-
+//The main BankAccount constructor that establishes the values of the attributes and methods of the class.
 BankAccount::BankAccount(std::string accountNumber, double balance, std::string ownerName, std::string accountType)
     : accountNumber(accountNumber), balance(balance), ownerName(ownerName), accountType(accountType) {}
 
+//Getters and setters are paired in order to have an organized view of the methods and their return values.
 std::string BankAccount::getAccountNumber() const {
     return accountNumber;
 }
@@ -35,7 +36,7 @@ std::string BankAccount::getAccountType() const {
 void BankAccount::setAccountType(std::string accountType) {
     accountType = accountType;
 }
-
+//These two following independent methods perform the addition or substraction of the amount given by the user to the original balance.
 void BankAccount::deposit(double amount) {
     balance += amount;
 }
