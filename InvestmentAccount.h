36e43ecab67c7f5ab2be78cea62ff331552ccf8f
nataliareyram <a01352603@tec.mx>
2024-11-22 22:19:31 -0600

    InvestmentAccount.h

--- InvestmentAccount.h
+++ InvestmentAccount.h
@@ -2,22 +2,22 @@
 #define INVESTMENTACCOUNT_H
 
 #include "BankAccount.h"
-
+//The subclass inherits the methods and attributes of its parent class, having then to establish the ones exclusive to this class.
 class InvestmentAccount : public BankAccount {
 private:
     double investmentBalance;
     double growthRate;
-
+//The constructor includes the parent class' attributes defined by their data type, as well as the sub class' own attributes and data types.
 public:
     InvestmentAccount(std::string accountNumber, double balance, std::string ownerName, std::string accountType, 
                       double investmentBalance, double growthRate);
-
+    //The sub class' getters and setters are paired together to organize the program.
     double getInvestmentBalance() const;
     void setInvestmentBalance(double investmentBalance);
 
     double getGrowthRate() const;
     void setGrowthRate(double growthRate);
-
+    //These methods perform operations on numeric values, but do not get or set information.
     void applyGrowth();
     void transferToInvestment(double amount);
     void withdrawFromInvestment(double amount);
